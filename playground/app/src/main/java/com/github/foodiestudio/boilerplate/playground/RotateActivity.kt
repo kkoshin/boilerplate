1571b87a8ec4d1919123bd123473fe348e00f290
Jiangc <jiangc@vistring.com>
2022-05-06 23:57:40 +0800

    feat: 处理resize & move的情况

--- playground/app/src/main/java/com/github/foodiestudio/boilerplate/playground/RotateActivity.kt
+++ playground/app/src/main/java/com/github/foodiestudio/boilerplate/playground/RotateActivity.kt
@@ -1,7 +1,9 @@
 package com.github.foodiestudio.boilerplate.playground
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -10,6 +12,7 @@ import androidx.core.view.*
 import com.github.foodiestudio.boilerplate.playground.utils.ScreenUtil
 import com.github.foodiestudio.boilerplate.playground.utils.logcat
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 private fun IntArray.safeGet(position: Int): Int {
     return when {
@@ -43,14 +46,26 @@ class RotateActivity : ComponentActivity(R.layout.activity_rotate) {
     private var indexOffset = 0
 
     private val contentView by lazy { findViewById<View>(R.id.ll) }
+    private val moveButton by lazy { findViewById<View>(R.id.imageButton_move) }
+    private val resizeButton by lazy { findViewById<View>(R.id.imageButton_resize) }
 
+    private var rotated = false
+
+    private var offsetX = 0f
+    private var offsetY = 0f
+
+    private val mClockWise = false
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         findViewById<Button>(R.id.btn_test).setOnClickListener {
-            rotate(true,false)
+            rotate(true, mClockWise, contentView.width, contentView.height)
+            rotated = true
         }
         findViewById<Button>(R.id.btn_test_2).setOnClickListener {
             reset()
+            rotated = false
         }
         contentView.apply {
             marginsArray = intArrayOf(
@@ -61,10 +76,63 @@ class RotateActivity : ComponentActivity(R.layout.activity_rotate) {
             )
         }
         indexOffset = 0
-        reset()
 
         findViewById<Button>(R.id.btn_xxx).setOnClickListener {
-            contentView.rotation = 270f
+            contentView.updateLayoutParams {
+                if (rotated) {
+                    height += _16dp
+                    contentView.translationX -= _16dp / 2
+                    contentView.translationY -= _16dp / 2
+                }
+            }
+        }
+
+        resizeButton.setOnTouchListener(SimpleDistanceChangeListener(onChange = { dx, dy ->
+            resize(dx, dy, mClockWise)
+        }))
+        moveButton.setOnTouchListener(SimpleDistanceChangeListener(onChange = {dx, dy ->
+            move(dx, dy)
+        }))
+    }
+
+    private fun move(deltaX: Float, deltaY: Float) {
+        offsetX += deltaX
+        offsetY += deltaY
+        contentView.apply {
+            translationX += deltaX
+            translationY += deltaY
+        }
+    }
+
+    /**
+     * 只需要处理一种横屏的情况，另一边其实差不多的情况，最多注意下垂直方向
+     * - 视觉上的缩放大小
+     *   1. 调整高度
+     *      - 默认：可以
+     *      - 旋转后：实际上对应的是调整宽度，然后需要加一定的translation 做抵消，因为中心点变了 TODO 恢复到竖屏后，左边宽度有点变小了
+     *   2. 调整宽度,默认是match_parent
+     *      - 默认：调整 marginRight 为主
+     *      - 旋转后：调整 height,然后加上 translation 做补偿
+     */
+    // TODO(Jiangc): clockWise 为 true 的情况下还需要取反
+    private fun resize(deltaX: Float, deltaY: Float, clockWise: Boolean = false) {
+        val max = contentView.width - contentView.marginStart
+        contentView.updateLayoutParams {
+            if (rotated) {
+                width += deltaY.roundToInt()
+                contentView.translationY += deltaY / 2
+
+                val dX = if (clockWise) deltaX else -deltaX
+                height += dX.roundToInt()
+                contentView.translationX -= dX / 2
+                contentView.translationY -= dX / 2
+            } else {
+                height += deltaY.roundToInt()
+                // 这里要用 updateMarginsRelative，不能是 updateMargins
+                (this as ViewGroup.MarginLayoutParams).updateMarginsRelative(
+                    end = (rightMargin - deltaX.roundToInt()).coerceIn(0, max)
+                )
+            }
         }
     }
 
@@ -80,50 +148,20 @@ class RotateActivity : ComponentActivity(R.layout.activity_rotate) {
     private val currentMarginBottom: Int
         get() = marginsArray.safeGet(indexOffset + 2)
 
+    // TODO(Jiangc): 横屏 resize 之后，在恢复，框的大小有偏差
     private fun reset() {
         indexOffset = 0
         contentView.apply {
             rotation = 0f
-            translationX = 0f
-            translationY = 0f
+            translationX = offsetX
+            translationY = offsetY
             layoutParams.apply {
-                height = _200dp // ViewGroup.LayoutParams.WRAP_CONTENT
-                width = ViewGroup.LayoutParams.MATCH_PARENT
-                (this as ViewGroup.MarginLayoutParams).setMargins(
-                    currentMarginStart,
-                    currentMarginTop,
-                    currentMarginEnd,
-                    currentMarginBottom,
-                )
+                height = width // ViewGroup.LayoutParams.WRAP_CONTENT
+                width = 0 // constraint 约束
             }
             requestLayout()
         }
     }
-
-    // 靠近底部的视图旋转，旋转后只有高会变化
-//    private fun rotateTest(clockWise: Boolean) {
-//        findViewById<View>(R.id.ll).apply {
-//            val h = height // wrap_content
-//            val w = width // match_parent
-//            val rotatedH =
-//                ScreenUtil.getScreenSize(this.context).x  // ViewGroup.LayoutParams.MATCH_PARENT
-//            val offset = rotatedH - h
-//            rotation = if (clockWise) 270f else 90f
-//            val distanceX = ((w - h - offset) / 2).toFloat()
-//            translationX = if (clockWise) distanceX else -distanceX
-//            translationY = ((rotatedH - w) / 2).toFloat()
-//            updateLayoutParams {
-//                height = rotatedH // 这里模拟旋转后尺寸变化
-//                width = w
-//            }
-//            invalidate()
-//
-//            ObjectAnimator.ofFloat(findViewById(R.id.btn_xxx), "rotation", -90f, 0f).apply {
-//                duration = 1000
-//                start()
-//            }
-//        }
-//    }
 
     /**
      * #============phone===========##                       #
@@ -137,21 +175,23 @@ class RotateActivity : ComponentActivity(R.layout.activity_rotate) {
     /**
      * @param archerToTop 初始时视图是否更接近屏幕顶部
      */
-    private fun rotate(archerToTop: Boolean, clockWise: Boolean) {
+    private fun rotate(archerToTop: Boolean, clockWise: Boolean, h_: Int, w_: Int) {
         // 因为视图原本的 h 不参与计算，以及 margin 也不会，所以在调整窗口大小以及位置的时候，尽量调整这些参数
         contentView.apply {
             val w = width
 
-            val h_ = width      // 旋转后的高度
-            val w_ = height     // 旋转后的宽度
+//            val h_ = width      // 旋转后的高度
+//            val w_ = height     // 旋转后的宽度
             rotation = if (clockWise) 270f else 90f
             val distanceY = ((h_ - w_) / 2).toFloat()
             if (archerToTop) {
-                translationY = -distanceY
+                translationY += -distanceY
+//                // 如果constraintHorizontal_bias="0"时，居左的话，需要加这个偏移
+//                translationX += -((w_ - h_) / 2).toFloat()
             } else {
                 val distanceX = ((w - h_) / 2).toFloat()
-                translationX = if (clockWise) distanceX else -distanceX
-                translationY = distanceY
+                translationX += if (clockWise) distanceX else -distanceX
+                translationY += distanceY
             }
 
             updateLayoutParams {
@@ -164,9 +204,37 @@ class RotateActivity : ComponentActivity(R.layout.activity_rotate) {
     }
 
     private fun playRotationAnimation() {
-        ObjectAnimator.ofFloat(findViewById<Button>(R.id.imageButton_close), "rotation", -60f, 0f).apply {
-            duration = 300
-            start()
+        ObjectAnimator.ofFloat(findViewById<Button>(R.id.imageButton_close), "rotation", -60f, 0f)
+            .apply {
+                duration = 300
+                start()
+            }
+    }
+}
+
+class SimpleDistanceChangeListener(
+    private val onChange: (Float, Float) -> Unit
+) : View.OnTouchListener {
+    private var downX = 0f
+    private var downY = 0f
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(v: View, motionEvent: MotionEvent): Boolean {
+        when (motionEvent.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                downX = motionEvent.rawX
+                downY = motionEvent.rawY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val deltaX = motionEvent.rawX - downX
+                val deltaY = motionEvent.rawY - downY
+                downX = motionEvent.rawX
+                downY = motionEvent.rawY
+                onChange(deltaX, deltaY)
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+            }
         }
+        return true
     }
 }
