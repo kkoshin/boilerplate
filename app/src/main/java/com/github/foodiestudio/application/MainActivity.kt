cab53e8e347b9d187997f4a4c6da9f2f0aaee746
Jiangc <jiangc@vistring.com>
2022-12-09 16:47:30 +0800

    empty project

---
+++ app/src/main/java/com/github/foodiestudio/application/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.github.foodiestudio.application
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.foodiestudio.application.theme.ApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
