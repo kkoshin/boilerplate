e10d77aec641d8bd869e931e5702ce08dbbc38b0
Jiangc <jiangc@vistring.com>
2023-03-31 18:14:20 +0800

    feat: add showkase

--- app/src/main/java/com/github/foodiestudio/application/MainActivity.kt
+++ app/src/main/java/com/github/foodiestudio/application/MainActivity.kt
@@ -3,28 +3,38 @@ package com.github.foodiestudio.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.airbnb.android.showkase.annotation.ShowkaseRoot
+import com.airbnb.android.showkase.annotation.ShowkaseRootModule
+import com.airbnb.android.showkase.models.Showkase
 import com.github.foodiestudio.application.theme.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val context = LocalContext.current
             ApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                        Button(onClick = { startActivity(Showkase.getBrowserIntent(context)) }) {
+                            Text("Browse widgets")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+@ShowkaseRoot
+class ShowcaseRootModule : ShowkaseRootModule
