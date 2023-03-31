a79818bc70ac146219aae89960f39ea8dfae248b
Jiangc <jiangc@vistring.com>
2023-03-31 18:14:20 +0800

    feat: add showkase

---
+++ app/src/main/java/com/github/foodiestudio/application/media/VideoPlayer.kt
@@ -0,0 +1,11 @@
+package com.github.foodiestudio.application.media
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "VideoPlayer", group = "Media components")
+@Composable
+fun VideoPlayer() {
+    Text(text = "Hello Video")
+}
