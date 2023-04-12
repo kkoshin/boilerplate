f95544001f2a2afe411bcc4dbf58da743fc0847e
Jiangc <jiangc@vistring.com>
2023-04-12 13:57:43 +0800

    fix: missing innerProgress update

--- common/src/commonMain/kotlin/com/github/foodiestudio/application/SliderAndList.kt
+++ common/src/commonMain/kotlin/com/github/foodiestudio/application/SliderAndList.kt
@@ -55,6 +55,7 @@ fun SliderAndList() {
             logcat {
                 "seekRequest"
             }
+            innerProgress = firstVisibleItemIndex.toProgress()
             progress = innerProgress
         }
     }
