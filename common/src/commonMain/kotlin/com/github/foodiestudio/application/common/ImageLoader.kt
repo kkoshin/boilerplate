d391791d0b851c5c7f85e2fc06d10c866a88265a
Jiangc <jiangc@vistring.com>
2023-08-08 12:11:45 +0800

    photo grid selection

--- common/src/commonMain/kotlin/com/github/foodiestudio/application/common/ImageLoader.kt
+++ common/src/commonMain/kotlin/com/github/foodiestudio/application/common/ImageLoader.kt
@@ -1,7 +1,11 @@
 package com.github.foodiestudio.application.common
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 
 @Composable
 expect fun rememberImagePainter(url: String): Painter
+
+@Composable
+expect fun AsyncImage(url: String, modifier: Modifier)
