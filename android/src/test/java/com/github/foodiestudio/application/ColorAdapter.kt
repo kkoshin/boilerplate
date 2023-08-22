8809d78973ebe881b37b68191d56a131c72d16c1
Jiangc <jiangc@vistring.com>
2023-08-22 15:03:07 +0800

    ignore field

--- android/src/test/java/com/github/foodiestudio/application/ColorAdapter.kt
+++ android/src/test/java/com/github/foodiestudio/application/ColorAdapter.kt
@@ -3,6 +3,7 @@ package com.github.foodiestudio.application
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import com.squareup.moshi.FromJson
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonQualifier
 import com.squareup.moshi.ToJson
 import java.util.Locale
@@ -26,6 +27,7 @@ class ComposeColorAdapter {
 annotation class HexColor
 
 class Rectangle(
+    @Json(ignore = true) // or @Transient
     val size: Int,
     @HexColor val color: Int
 )
