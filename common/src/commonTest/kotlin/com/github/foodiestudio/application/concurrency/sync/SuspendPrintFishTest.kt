a3041914dd167d676d35e58ae522ee1115c38237
KoShin <koshin.1116@gmail.com>
2024-04-08 22:31:50 +0800

    test: NaiveAsyncPrintTest
    using spinLock to finish tests

---
+++ common/src/commonTest/kotlin/com/github/foodiestudio/application/concurrency/sync/SuspendPrintFishTest.kt
@@ -0,0 +1,25 @@
+package com.github.foodiestudio.application.concurrency.sync
+
+import org.junit.Test
+
+/**
+ * [PrintFishTest] 的协程版本
+ */
+class SuspendPrintFishTest {
+
+    /**
+     * 打印出 <><_<><_<><
+     */
+    @Test
+    fun `print fish V1`() {
+        TODO("")
+    }
+
+    /**
+     * 打印出 <><_><>_<><
+     */
+    @Test
+    fun `print fish V1_5`() {
+        TODO("")
+    }
+}
